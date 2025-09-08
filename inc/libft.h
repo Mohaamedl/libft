5682915fbdac3114f064f92e8f70bb18dde5ce8c
Mohamed Haddadi <94742483+Mohaamedl@users.noreply.github.com>
2025-09-08 19:23:45 +0100

    New42 (#2)
    * new libft with printf and get_next_line
    
    * Added printf and get_next_line into libft
    
    * Added atol strcmp and isspace
    
    * fix

--- inc/libft.h
+++ inc/libft.h
@@ -48,6 +48,7 @@ int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
+int				ft_isspace(int c);
 /* String functions */
 
 size_t			ft_strlen(const char *s);
@@ -57,7 +58,7 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlcat(char *dst, const char *src, size_t n);
-
+int				ft_strcmp(const char *s1, const char *s2);
 /* Memory functions */
 void			*ft_memset(void *s, int c, size_t n);
 void			ft_bzero(void *s, size_t n);
@@ -84,6 +85,7 @@ char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
 char			**ft_split(char const *s, char c);
 int				ft_atoi(const char *nptr);
+long			ft_atol(const char *nptr);
 char			*ft_itoa(int n);
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void			ft_striteri(char *s, void (*f)(unsigned int, char*));
@@ -100,10 +102,10 @@ void			ft_putnbr_fd(int n, int fd);
 
 /* Linked list management */
 t_list			*ft_lstnew(void *content);
-void			ft_lstadd_front(t_list **lst, t_list *new);
+void			ft_lstadd_front(t_list **lst, t_list *newl);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstadd_back(t_list **lst, t_list *newl);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
